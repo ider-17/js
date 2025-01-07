eb69f6f4105e631a4391338058372787d0555ba2
24LP6042 24LP6042 <24LP6042@G303-CC0142.local>
2025-01-07 12:45:19 +0800

    2025 01 07 object

---
+++ object.js
@@ -0,0 +1,28 @@
+// const person = {
+//     name: "ider",
+//     age: 20,
+//     city: "UB"
+// }
+
+
+
+// const person = new Object ();
+//     person.name = "ider";
+//     person.age = 20;
+//     person.city = "UB";
+
+
+
+const car = {
+    brand: "Toyota",
+    color: "Black",
+    mile: "200",
+    year: 2020,
+    price: 20000
+}
+
+function cars() {
+    for (let i = 0; i < car.lenght; i++) {
+        console.log(i);
+    }
+}
