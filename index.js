dc2c0324d5840f73eb7c55c6f0e68195726408d0
24LP6042 24LP6042 <24LP6042@G303-CC0142.local>
2025-01-09 12:58:47 +0800

    2025 01 09

--- index.js
+++ index.js
@@ -180,17 +180,128 @@
 
 
 
-const nums = [2, 7, 11, 15];
-const target = 9;
+// const nums = [2, 7, 11, 15];
+// const target = 9;
 
-// not use nested loop
+// // not use nested loop
 
-const obj = {};
+// const obj = {};
 
-for (let i = 0; i < nums.length; i++) {
-    obj[target - nums[i]] = i
+// for (let i = 0; i < nums.length; i++) {
+//     obj[target - nums[i]] = i
 
-    if (obj.hasOwnProperty(nums[i])){
-        console.log([obj[nums[i]], i]);
+//     if (obj.hasOwnProperty(nums[i])){
+//         console.log([obj[nums[i]], i]);
+//     }
+// }
+
+
+
+const weather = [
+    {
+        "date": "2024-12-26",
+        "temperature": { "current": -18.7, "min": -22.9, "max": -14.5 }
+    },
+    {
+        "date": "2024-12-27",
+        "temperature": { "current": -19.2, "min": -24.0, "max": -14.4 }
+    },
+    {
+        "date": "2024-12-28",
+        "temperature": { "current": -17.4, "min": -21.2, "max": -13.5 }
+    },
+    {
+        "date": "2024-12-29",
+        "temperature": { "current": -16.4, "min": -22.0, "max": -10.7 }
+    },
+    {
+        "date": "2024-12-30",
+        "temperature": { "current": -15.7, "min": -20.5, "max": -10.9 }
+    },
+    {
+        "date": "2024-12-31",
+        "temperature": { "current": -17.4, "min": -22.7, "max": -12.1 }
+    },
+    {
+        "date": "2025-01-01",
+        "temperature": { "current": -20.4, "min": -23.9, "max": -16.8 }
+    },
+    {
+        "date": "2025-01-02",
+        "temperature": { "current": -20.1, "min": -25.8, "max": -14.3 }
+    },
+    {
+        "date": "2025-01-03",
+        "temperature": { "current": -17.8, "min": -23.5, "max": -12.0 }
+    },
+    {
+        "date": "2025-01-04",
+        "temperature": { "current": -18.3, "min": -22.4, "max": -14.2 }
+    },
+    {
+        "date": "2025-01-05",
+        "temperature": { "current": -16.8, "min": -22.0, "max": -11.6 }
+    },
+    {
+        "date": "2025-01-06",
+        "temperature": { "current": -16.6, "min": -22.4, "max": -10.7 }
+    },
+    {
+        "date": "2025-01-07",
+        "temperature": { "current": -15.9, "min": -22.2, "max": -9.6 }
+    },
+    {
+        "date": "2025-01-08",
+        "temperature": { "current": -15.6, "min": -21.9, "max": -9.3 }
     }
-}
+]
+
+// 1.1 Descenting sort
+// 1.2 Last week weather informationconst 
+// 2.1 - Ascending sort names second letter// 
+// 2.2 - Names group by first letter
+
+// 1.1
+
+const sortedWeather = weather.sort((a, b) => new Date(b.date) - new Date(a.date))
+
+console.log(sortedWeather);
+
+
+
+// 1.2
+
+
+
+// // 2.1
+
+// const names = [
+//     "baljan",
+//     "itgel",
+//     "jamiyansuren",
+//     "chingun",
+//     "telmuun",
+//     "chinguun",
+//     "nomi",
+//     "aygul",
+//     "behbat",
+//     "narangerel",
+//     "oyunbat",
+//     "turbat",
+//     "baasandash",
+//     "jargalsaihan",
+//     "tugsbileg",
+//     "huwituguldur",
+//     "ursaa",
+//     "ider",
+//     "ariunjargal"
+//   ];
+
+// const sortedNames = names.sort((a,b) => {
+//     const sortedNamesA = a[1].toLowerCase() || '';
+//     const sortedNamesB = b[1].toLowerCase() || '';
+
+//     return sortedLatterA.localeCompare(sortedNamesB);
+
+// })
+// console.log(sortedNames);
