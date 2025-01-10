ae6f7d70bc96eb4f251fefcf2eea2ef5fe08c017
24LP6042 24LP6042 <24LP6042@G303-CC0142.local>
2025-01-10 13:46:36 +0800

    2025-01-10

--- index.js
+++ index.js
@@ -197,64 +197,64 @@
 
 
 
-const weather = [
-    {
-        "date": "2024-12-26",
-        "temperature": { "current": -18.7, "min": -22.9, "max": -14.5 }
-    },
-    {
-        "date": "2024-12-27",
-        "temperature": { "current": -19.2, "min": -24.0, "max": -14.4 }
-    },
-    {
-        "date": "2024-12-28",
-        "temperature": { "current": -17.4, "min": -21.2, "max": -13.5 }
-    },
-    {
-        "date": "2024-12-29",
-        "temperature": { "current": -16.4, "min": -22.0, "max": -10.7 }
-    },
-    {
-        "date": "2024-12-30",
-        "temperature": { "current": -15.7, "min": -20.5, "max": -10.9 }
-    },
-    {
-        "date": "2024-12-31",
-        "temperature": { "current": -17.4, "min": -22.7, "max": -12.1 }
-    },
-    {
-        "date": "2025-01-01",
-        "temperature": { "current": -20.4, "min": -23.9, "max": -16.8 }
-    },
-    {
-        "date": "2025-01-02",
-        "temperature": { "current": -20.1, "min": -25.8, "max": -14.3 }
-    },
-    {
-        "date": "2025-01-03",
-        "temperature": { "current": -17.8, "min": -23.5, "max": -12.0 }
-    },
-    {
-        "date": "2025-01-04",
-        "temperature": { "current": -18.3, "min": -22.4, "max": -14.2 }
-    },
-    {
-        "date": "2025-01-05",
-        "temperature": { "current": -16.8, "min": -22.0, "max": -11.6 }
-    },
-    {
-        "date": "2025-01-06",
-        "temperature": { "current": -16.6, "min": -22.4, "max": -10.7 }
-    },
-    {
-        "date": "2025-01-07",
-        "temperature": { "current": -15.9, "min": -22.2, "max": -9.6 }
-    },
-    {
-        "date": "2025-01-08",
-        "temperature": { "current": -15.6, "min": -21.9, "max": -9.3 }
-    }
-]
+// const weather = [
+//     {
+//         "date": "2024-12-26",
+//         "temperature": { "current": -18.7, "min": -22.9, "max": -14.5 }
+//     },
+//     {
+//         "date": "2024-12-27",
+//         "temperature": { "current": -19.2, "min": -24.0, "max": -14.4 }
+//     },
+//     {
+//         "date": "2024-12-28",
+//         "temperature": { "current": -17.4, "min": -21.2, "max": -13.5 }
+//     },
+//     {
+//         "date": "2024-12-29",
+//         "temperature": { "current": -16.4, "min": -22.0, "max": -10.7 }
+//     },
+//     {
+//         "date": "2024-12-30",
+//         "temperature": { "current": -15.7, "min": -20.5, "max": -10.9 }
+//     },
+//     {
+//         "date": "2024-12-31",
+//         "temperature": { "current": -17.4, "min": -22.7, "max": -12.1 }
+//     },
+//     {
+//         "date": "2025-01-01",
+//         "temperature": { "current": -20.4, "min": -23.9, "max": -16.8 }
+//     },
+//     {
+//         "date": "2025-01-02",
+//         "temperature": { "current": -20.1, "min": -25.8, "max": -14.3 }
+//     },
+//     {
+//         "date": "2025-01-03",
+//         "temperature": { "current": -17.8, "min": -23.5, "max": -12.0 }
+//     },
+//     {
+//         "date": "2025-01-04",
+//         "temperature": { "current": -18.3, "min": -22.4, "max": -14.2 }
+//     },
+//     {
+//         "date": "2025-01-05",
+//         "temperature": { "current": -16.8, "min": -22.0, "max": -11.6 }
+//     },
+//     {
+//         "date": "2025-01-06",
+//         "temperature": { "current": -16.6, "min": -22.4, "max": -10.7 }
+//     },
+//     {
+//         "date": "2025-01-07",
+//         "temperature": { "current": -15.9, "min": -22.2, "max": -9.6 }
+//     },
+//     {
+//         "date": "2025-01-08",
+//         "temperature": { "current": -15.6, "min": -21.9, "max": -9.3 }
+//     }
+// ]
 
 // 1.1 Descenting sort
 // 1.2 Last week weather informationconst 
@@ -263,17 +263,26 @@ const weather = [
 
 // 1.1
 
-const sortedWeather = weather.sort((a, b) => new Date(b.date) - new Date(a.date))
+// const sortedWeather = weather.sort((a, b) => new Date(b.date) - new Date(a.date))
 
-console.log(sortedWeather);
+// console.log(sortedWeather);
 
 
 
 // 1.2
 
+// const today = new Date();
+// const currentWeekMonday = new Date();
+
+// currentWeekMonday.setDate(today.getDate() - today.getDay())
+
+// const sliced = weather.filter(element => {
+//     return new Date(element.date) > currentWeekMonday
+// })
 
 
-// // 2.1
+
+// 2.1
 
 // const names = [
 //     "baljan",
@@ -305,3 +314,59 @@ console.log(sortedWeather);
 
 // })
 // console.log(sortedNames);
+
+// // 2.2
+
+// const obj ={};
+
+// names.forEach((element) => {
+//     if (obj.hasOwnProperty(element[0])) {
+//         obj[element[0]].push(element)
+//     } else {
+//         obj[element[0]] = [element]
+//     }
+// });
+
+// console.log(obj);
+
+
+
+// Contains Duplicate
+
+// 1
+
+const nums = [1,2,3,1];
+function containsDuplicate(nums) {
+
+for (let i = 0; i < nums.length; i++) {
+    for (let j = i+1; j < nums.length; j++) {
+        if (nums[i] == nums[j]) {
+            return true
+        } 
+        }
+    }    
+}
+
+containsDuplicate(nums = [1,2,3,1])
+
+
+// jishee
+
+// const x = 90;
+// const y = 10;
+//  let a = 0;
+// if (x < y) {
+//     a = true
+// } else {
+//     a = false
+// }
+
+// console.log(a);
+
+// 3
+
+// const nums = [1,2,3,1];
+
+// for (let i = 0; i < nums.length; i++) {
+
+// }
